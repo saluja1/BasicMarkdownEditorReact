162e9d99cb8003a9729d746ce7e55993750f1562
saluja1 <amit.saluja.106@gmail.com>
2020-12-11 16:53:19 +0530

    Basic MarkDown Editor

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MarkdownEditor from './MarkdownEditor.js';
+
+
+ReactDOM.render(
+    <MarkdownEditor />,
+    document.getElementById('root')
+);
